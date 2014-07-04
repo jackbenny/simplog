56da0c396ad331c4066555f2f5f8633e77db103c
Jack-Benny Persson <jack-benny@cyberinfo.se>
2014-07-04 20:10:54 +0200

    Deleted obsolete file updatefile.php and rewrote editfile.php completly

--- user/editfile.php
+++ user/editfile.php
@@ -2,33 +2,22 @@
 require ("../includes/content.php");
 require ("../includes/miscfunc.php");
 require ("../includes/htmlcode.php");
-$filename = $_GET['file'];
-$file = Page::$parentDir . Page::$contentFolder . $filename;
 
-$filehandle = fopen ("$file", "rw");
-$content = fread($filehandle, filesize($file));
+//Make some form of control below, this is unsafe...
+$file = Page::$parentDir . Page::$contentFolder . $_GET['file'];
 
+if(isset($_POST['content']))
+{
+        $postedContent = $_POST['content']; 
+        file_put_contents($file, $postedContent);
+}
 start_html("Edit file");
-print "<form action=\"$_SERVER[PHP_SELF]\" method='post'>
-Text: <br />
-<textarea cols='80' rows='30' name='file'>$content</textarea>
-<br />
-<input type='submit' value='Save file'>
-</form>
-";
+print "<form action=\"\" method=\"post\">\n";
+$content = file_get_contents($file);
+print "<textarea name='content' cols='80' rows='30'>\n" . ($content) . "\n</textarea>";
+print "<br/>\n
+<input type=\"submit\" value=\"Save file\"/>
+</form>";
+end_html();
 
-if (!isset($_POST['post']))
-{
-    end_html();
-    die;
-}
-
-if (fwrite($filehandle, $_POST['post']) === FALSE) 
-{
-    echo "Cannot write to file ($file)";
-    exit;
-}
-
-echo "Success, saved file ($file)";
-fclose($filehandle);
 ?>
